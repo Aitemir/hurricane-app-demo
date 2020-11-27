5075f2fc84a2fdfcc7c8be91850eb12eb22606ff
Aitemir Yeskenov <aitemir@Aitemirs-MacBook-Pro.local>
2020-11-27 13:51:57 -0600

    enhanced CSS of the components, fixed bugs

--- src/app/evacuee/shelter/shelter.component.ts
+++ src/app/evacuee/shelter/shelter.component.ts
@@ -45,8 +45,8 @@ export class ShelterComponent implements OnInit {
 
   getAvailableShelters() {
     const promise = new Promise<Shelter[]>((resolve, reject) => {
-      //const apiURL = "api/shelters";
-      const apiURL = "https://hurricane-app-be-demo.azurewebsites.net/shelters";
+      const apiURL = "api/shelters";
+      //const apiURL = "https://hurricane-app-be-demo.azurewebsites.net/shelters";
       this.http
         .get<Shelter[]>(apiURL)
         .pipe(timeout(5000))
